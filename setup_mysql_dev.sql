e819e4246adcf16e055badab60918e6cf1d143d7
Bart254 <omondibartlett@gmail.com>
2024-01-28 00:18:05 +0300

    update setup_mysql_dev.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -6,4 +6,4 @@ CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost'
 IDENTIFIED BY 'hbnb_dev_pwd';
 -- Grants all privileges on db to the user
 GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT on perfomance_schema.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT on performance_schema.* TO 'hbnb_dev'@'localhost';
