f9b7320b74895189b9cbb4490abfde9a6e941ded
Bart254 <omondibartlett@gmail.com>
2024-02-23 13:58:35 +0300

    add capture param

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,11 +8,11 @@ from datetime import datetime
 def do_pack():
     """ Function uses fabric to pack local web_static files
     """
-    r1 = local("mkdir -p versions")
+    r1 = local("mkdir -p versions", capture=True)
     archive_file = 'web_static' + datetime.now().strftime('%Y%m%d%H%M%S') +\
         '.tgz'
     archive_path = 'versions' + '/' + archive_file
-    r2 = local("tar -cvzf {} web_static".format(archive_path))
+    r2 = local("tar -cvzf {} web_static".format(archive_path), capture=True)
     if r1.failed or r2.failed:
         return None
     return archive_path
